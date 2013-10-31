76332059a708b2ec192219f73fb5fc9cdf826482
swelly127 <jessicashu127@gmail.com>
2013-10-30 20:13:57 -0400

    test

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,10 @@ var Task = mongoose.model("Task");
 var utils = require('utils');
 
 exports.addtask = function(req, res){
+  console.log("date we have is");
+  console.log(res.body.date);
+  console.log("we made");
+  console.log(utils.parse_date(req.body.date));
   if (req.body.content.trim()!=""){
     var temp = new Task({
       content: req.body.content.trim(),
@@ -16,7 +20,8 @@ exports.addtask = function(req, res){
       finished_date: null,
       due_date: utils.parse_date(req.body.date)
     }).save(function(err, task){
-      if (err) { console.log("task not saved - error"); }
+      if (err) { console.log("task not saved - error");
+      console.log(err); }
       else {
       	req.users.tasks.push(task)
       	console.log("LOOKATME!!")
